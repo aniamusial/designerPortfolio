d8658f71ac134546e46710dd4a9e4f3062a72d96
Anna Musial <ania.musial13@gmail.com>
2019-01-26 16:42:35 +0100

    Initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-toolbar color="transparent" flat>   
+    <img src="../assets/logo.png" height="60%"/>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>Projects</v-btn>
+        <v-btn flat>About</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-divider id="divider"></v-divider>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'NavBar'
+}
+</script>
+
+<style scoped>
+    div {
+        font-family: "Quicksand", sans-serif;
+    }
+
+    #divider {
+        border: 1px solid #000;
+    }
+    
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import Router from 'vue-router'
+
+Vue.use(Vuetify);
+Vue.use(Router);
 
 Vue.config.productionTip = false
 
